a3a27b0fdcc648ea8916a8dc92b89fdbeb619724
boscaiolo <boscaiolo.gp@gmail.com>
2019-11-03 00:13:02 +0000

    adding one to many - TODO a hacky way to be able to display in thymeleaf

--- src/main/java/com/example/dogs/Breed.java
+++ src/main/java/com/example/dogs/Breed.java
@@ -1,8 +1,11 @@
 package com.example.dogs;
 
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Breed {
@@ -10,6 +13,25 @@ public class Breed {
         @Id
         @NotBlank(message = "Name is mandatory")
         private String name;
+
+        @ElementCollection
+        private Set<String> subBreeds = new HashSet<>();
+
+        public Set<String> getSubBreeds() {
+                return subBreeds;
+        }
+
+        public void setSubBreeds(Set<String> subBreeds) {
+                this.subBreeds = subBreeds;
+        }
+
+        public void setSubBreed(String subBreed) {
+                this.subBreeds.add(subBreed.toUpperCase());
+        }
+
+        public String getSubBreed() {
+                return this.subBreeds.toString();
+        }
 
         public String getName() {
                 return name;
