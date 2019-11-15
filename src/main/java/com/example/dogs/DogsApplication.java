841573ba90589893a76f76a0114ca8ca518043cf
boscaiolo <boscaiolo.gp@gmail.com>
2019-11-15 22:19:17 +0000

    fine tunning security to always display the list

--- src/main/java/com/example/dogs/DogsApplication.java
+++ src/main/java/com/example/dogs/DogsApplication.java
@@ -15,7 +15,9 @@ public class DogsApplication {
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
 			http.authorizeRequests()
-					.anyRequest().authenticated()
+					.antMatchers("/", "/index").permitAll()
+					.anyRequest()
+					.authenticated()
 					.and()
 					.oauth2Login();
 		}
